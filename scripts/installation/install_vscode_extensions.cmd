f5a642f333f554de59a61fe774f87b935af0329d
Fabien Farella <f.farella@ews-consulting.at>
2018-08-27 14:58:50 +0200

    Revert to VSCode instead of code insiders

--- scripts/installation/install_vscode_extensions.cmd
+++ scripts/installation/install_vscode_extensions.cmd
@@ -1,23 +1,23 @@
 @echo off
 @REM START VSCODE
-call "%~dp0\start_ews_env.cmd"
+call "%~dp0\..\start_ews_env.cmd"
 
 @REM Copy the settings
 echo D | xcopy /y "%EWS_CONFIG_DIR%\code_settings.json" "%EWS_CONFIG_DIR%\code\User\settings.json"
 
 @REM Install the extensions
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension christian-kohler.path-intellisense
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension dbaeumer.vscode-eslint
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension formulahendry.docker-explorer
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension HookyQR.beautify
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension jhartell.vscode-line-endings
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension McCarter.start-git-bash
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension ms-python.anaconda-extension-pack
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension ms-python.python
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension PeterJausovec.vscode-docker
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension PKief.material-icon-theme
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension redhat.vscode-yaml
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension robertohuertasm.vscode-icons
-call "%~dp0\start_code_insiders_py27.cmd" --install-extension Zim.vsc-docker
+call "%~dp0\..\start_code_py27.cmd" --install-extension christian-kohler.path-intellisense
+call "%~dp0\..\start_code_py27.cmd" --install-extension dbaeumer.vscode-eslint
+call "%~dp0\..\start_code_py27.cmd" --install-extension formulahendry.docker-explorer
+call "%~dp0\..\start_code_py27.cmd" --install-extension HookyQR.beautify
+call "%~dp0\..\start_code_py27.cmd" --install-extension jhartell.vscode-line-endings
+call "%~dp0\..\start_code_py27.cmd" --install-extension McCarter.start-git-bash
+call "%~dp0\..\start_code_py27.cmd" --install-extension ms-python.anaconda-extension-pack
+call "%~dp0\..\start_code_py27.cmd" --install-extension ms-python.python
+call "%~dp0\..\start_code_py27.cmd" --install-extension PeterJausovec.vscode-docker
+call "%~dp0\..\start_code_py27.cmd" --install-extension PKief.material-icon-theme
+call "%~dp0\..\start_code_py27.cmd" --install-extension redhat.vscode-yaml
+call "%~dp0\..\start_code_py27.cmd" --install-extension robertohuertasm.vscode-icons
+call "%~dp0\..\start_code_py27.cmd" --install-extension Zim.vsc-docker
 
 
--- scripts/start_code_py27.cmd
+++ scripts/start_code_py27.cmd
@@ -3,10 +3,10 @@ call "%~dp0\start_ews_env.cmd"
 call "%~dp0\start_conda_env_py27.cmd"
 REM call "%~dp0\start_docker_env.cmd"
 setlocal
-set EWS_VSCODE_PATH=%EWS_BIN_DIR%/VSCode-insider
+set EWS_VSCODE_PATH=%EWS_BIN_DIR%/VSCode
 set EWS_VSCODE_EXT_PATH=%EWS_CONFIG_DIR%\code_extensions
 set EWS_VSCODE_DATA_PATH=%EWS_CONFIG_DIR%\code
 set VSCODE_DEV=
 set ELECTRON_RUN_AS_NODE=1
-call "%EWS_VSCODE_PATH%\Code - Insiders.exe" "%EWS_VSCODE_PATH%\resources\app\out\cli.js"  --extensions-dir="%EWS_VSCODE_EXT_PATH%" --user-data-dir="%EWS_VSCODE_DATA_PATH%"  %*
+call "%EWS_VSCODE_PATH%\Code.exe" "%EWS_VSCODE_PATH%\resources\app\out\cli.js"  --extensions-dir="%EWS_VSCODE_EXT_PATH%" --user-data-dir="%EWS_VSCODE_DATA_PATH%"  %*
 endlocal
